53dbd7271ec6366a3475dcd216d0e0018083b599
Anirudh <anirudhc@berkeley.edu>
2023-09-28 18:35:24 -0700

    informatics code

---
+++ informatics/vitamin.py
@@ -0,0 +1,77 @@
+"""
+IMPORT STATEMENTS?
+What do we need to import and how should we import it?
+"""
+
+"""
+Calculate the sum of all even numbers from 1 to 100 and store the result in a variable called even_sum.
+"""
+def q1():
+    print("Question 1: Not Implemented")
+    return
+
+"""
+Given the following list of data points: [12, 15, 18, 21, 24], create a numpy array and calculate the mean, median, and standard deviation of these values. Store the results in separate variables.
+"""
+def q2():
+    print("Question 2: Not Implemented")
+    return
+
+"""
+Perform matrix multiplication on two numpy arrays: A (2x3 matrix) and B (3x2 matrix). Store the result in a variable called result_matrix.
+"""
+def q3():
+    print("Question 3: Not Implemented")
+    return
+
+
+"""
+Generate two random numpy arrays, data1 and data2, each with 100 samples.
+Conduct a t-test to compare the means of data1 and data2.
+Print the p-value from the t-test.
+"""
+
+def q4():
+    print("Question 4: Not Implemented")
+    return
+
+
+"""
+1. Create 4 Lists with 5 elements in each:
+Name 
+Age 
+Gender
+Score
+
+2. Create a dictionary from these fours lists
+    Create a pandas DataFrame from a dictionary with columns: 'Name', 'Age', 'Gender', 'Score'.
+
+3. Calculate the mean age and the maximum score in the DataFrame.
+4. Plot a bar chart showing the count of each gender.
+"""
+def q5():
+    print("Question 5: Not Implemented")
+    return
+
+
+"""
+Load a dataset (students.csv) into a pandas DataFrame.
+Conduct a hypothesis test to compare a numerical variable between two or more groups in the dataset.
+Use matplotlib to create appropriate visualizations (e.g., box plot, histogram) to represent the data.
+Provide a summary of the hypothesis test results and your visualizations, explaining any insights gained from the analysis.
+"""
+
+def q6():
+    print("Question 6: Not Implemented")
+    return
+
+def main():
+    q1()
+    q2()
+    q3()
+    q4()
+    q5()
+    q6()
+
+if __name__ == "__main__":
+    main()
